acf2fa596ebee90019874dc08c98a3942adbe426
Alex Aguirre <teamaguirre@gmail.com>
2022-09-10 12:23:40 -0700

    Initial Commit

---
+++ src/components/Gallery.js
@@ -0,0 +1,11 @@
+import GalleryItem from './GalleryItem'
+
+function Gallery(props){
+    return (
+        <div>
+            <GalleryItem title={title} />
+        </div>
+    )
+}
+
+export default Gallery
